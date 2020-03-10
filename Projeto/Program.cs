e305e5a336336b2f1bacbb53888d4acadd149f90
Gabie Lino <linogmgabriela@gmail.com>
2020-03-10 17:43:25 -0300

    Adicionamento do exercicio

---
+++ Projeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string [] comidas = { "coxinha", "bolinha de queijo", "pão de queijo"};
+
+            foreach(string comida in comidas)
+            {
+                Console.WriteLine($"As comidas são: {comida}") ;
+            }
+
+            Console.ReadKey();    
+   
+        }
+    }
+}
